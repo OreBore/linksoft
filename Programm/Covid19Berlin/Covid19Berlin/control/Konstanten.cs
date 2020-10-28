f98e9b2783af6ac942597ca7e7ea7d9b7e4984a0
SteveDruPrv <69581528+SteveDruPrv@users.noreply.github.com>
2020-10-28 09:25:57 +0100

    Add files via upload
    Final

---
+++ Programm/Covid19Berlin/Covid19Berlin/control/Konstanten.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covid19Berlin.control
+{
+    class Konstanten
+    {
+        public const string FALLZAHL = "fallzahl";
+        public const string DIFFERENZ = "differenz";
+        public const string INZIDENZ = "inzidenz";
+        public const string GENESEN = "genesen";
+        public const string ID = "id";
+        public const string BEZIRK = "bezirk";
+        public const string DEFAULTXML = "../../ressources/statistikdaten.xml";
+        
+       
+    }
+}
